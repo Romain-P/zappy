c78eede88b02c6c508c12ead392ff287edec10ff
Romain-P <romain.pillot@epitech.eu>
2018-06-22 11:12:22 +0200

    fixed time handling: now dynamic, can be changed at runtime

--- server/src/time_handler.c
+++ server/src/time_handler.c
@@ -6,26 +6,29 @@
 
 static size_t const MAX_COMMANDS = 10;
 static time_t exec_time;
+static int tic_time;
 
 int to_seconds(int tics) {
     return tics / server.freq;
 }
 
 static bool inserter(waiting_t *waiting) {
-    return exec_time < waiting->exec_time;
+    return exec_time < waiting->start_time + ( to_seconds(waiting->tics) * 1000 );
 }
 
 bool delay(void *packet, handler_t handler, player_t *player, int tics) {
     if (player->waiting_commands >= MAX_COMMANDS)
         return false;
     ((network_packet_t *) packet)->delayed = true;
-    exec_time = time(NULL) + to_seconds(tics) * 1000;
+    exec_time = time(NULL);
+    tic_time = tics;
     player->waiting_commands++;
     waiting_t *waiting = malloc(sizeof(*waiting));
     waiting->player = player;
-    waiting->exec_time = exec_time;
+    waiting->start_time = exec_time;
     waiting->command_handler = handler;
     waiting->packet = packet;
+    waiting->tics = tic_time;
     list_insert(&server.pending, waiting, (predicate_t) &inserter);
     return true;
 }
@@ -37,7 +40,7 @@ void check_delayed_tasks() {
         waiting_t *waiting = it->data;
         iter_next(it);
 
-        if (current_time >= waiting->exec_time) {
+        if (current_time >= waiting->start_time + (to_seconds(waiting->tics) * 1000)) {
             waiting->command_handler(waiting->player, waiting->packet);
             list_remove(&server.pending, waiting);
             waiting->player->waiting_commands--;
