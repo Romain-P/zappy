9793f3c44e1bbf1cd544c2970af524018f9e3877
Kilian MAYANS <kilian.mayans@epitech.eu>
2018-06-24 16:33:02 +0000

    correc norme network_epoll.c

--- shared/src/client_handler.c
+++ shared/src/client_handler.c
@@ -1,6 +1,9 @@
-//
-// Created by romain on 26/05/18.
-//
+/*
+** EPITECH PROJECT, 2018
+** client_handler
+** File description:
+** client_handler.c
+*/
 
 #include <unistd.h>
 #include "network.h"
@@ -9,34 +12,35 @@
 #include "util.h"
 
 static void on_connect(network_client_t *client) {
-    eprintf("[Client %d] connected to [Server]\n", client->id);
-    zappy_instance.gui_handlers.on_connect(client->id);
+	eprintf("[Client %d] connected to [Server]\n", client->id);
+	zappy_instance.gui_handlers.on_connect(client->id);
 }
 
 static void on_disconnect(network_client_t *client) {
-    zappy_instance.gui_handlers.on_disconnect(client->id);
-    eprintf("[Client %d] disconnected to server\n", client->id);
+	zappy_instance.gui_handlers.on_disconnect(client->id);
+	eprintf("[Client %d] disconnected to server\n", client->id);
 }
 
-static void on_received(network_client_t *client, char const *packet, size_t len) {
-    eprintf("[Client %d] Recv \t<--\t[Server]:\t\t", client->id);
-    fflush(stderr);
-    write(STDERR_FILENO, packet, len);
-    eprintf("\n");
+static void on_received(network_client_t *client, char const *packet,
+			size_t len) {
+	eprintf("[Client %d] Recv \t<--\t[Server]:\t\t", client->id);
+	fflush(stderr);
+	write(STDERR_FILENO, packet, len);
+	eprintf("\n");
 
-    parse_packet(client, packet, len);
+	parse_packet(client, packet, len);
 }
 
 static void on_sent(network_client_t *client, char const *packet, size_t len) {
-    eprintf("[Client %d] Sent \t-->\t[Server]:\t\t", client->id);
-    fflush(stderr);
-    write(STDERR_FILENO, packet, len - 1);
-    eprintf("\n");
+	eprintf("[Client %d] Sent \t-->\t[Server]:\t\t", client->id);
+	fflush(stderr);
+	write(STDERR_FILENO, packet, len - 1);
+	eprintf("\n");
 }
 
 void configure_client_handler(client_handler_t *handler) {
-    handler->on_received = &on_received;
-    handler->on_connect = &on_connect;
-    handler->on_sent = &on_sent;
-    handler->on_disconnect = &on_disconnect;
+	handler->on_received = &on_received;
+	handler->on_connect = &on_connect;
+	handler->on_sent = &on_sent;
+	handler->on_disconnect = &on_disconnect;
 }
