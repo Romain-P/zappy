c08614731db73b79434f3c6c1f767fedee5ca6ee
Wilka <wilka.sombrun@epitech.eu>
2018-07-01 22:15:19 +0200

    Fix norme

--- shared/include/lib.h
+++ shared/include/lib.h
@@ -1,31 +1,34 @@
-//
-// Created by romain on 20/06/18.
-//
+/*
+** EPITECH PROJECT, 2018
+** LIB
+** File description:
+** ZAPPY
+*/
 
 #ifndef ZAPPY_LIB_H
-#define ZAPPY_LIB_H
+	#define ZAPPY_LIB_H
 
-#include "protocol.h"
+	#include "protocol.h"
 
-typedef struct zappy_instance_s zappy_instance_t;
+	typedef struct zappy_instance_s zappy_instance_t;
 
-struct zappy_instance_s {
-    network_instance_t net;
-    network_config_t config;
-    gui_handlers_t gui_handlers;
-    ai_handlers_t ai_handlers;
-    list_t pending;
-    bool thread_sync;
-    pthread_mutex_t locker;
-    bool connected;
-    uint32_t onDisconnect;
-    uint32_t onConnect;
-};
+	struct zappy_instance_s {
+		network_instance_t net;
+		network_config_t config;
+		gui_handlers_t gui_handlers;
+		ai_handlers_t ai_handlers;
+		list_t pending;
+		bool thread_sync;
+		pthread_mutex_t locker;
+		bool connected;
+		uint32_t onDisconnect;
+		uint32_t onConnect;
+	};
 
-extern zappy_instance_t zappy_instance;
+	extern zappy_instance_t zappy_instance;
 
-void configure_client_handler(client_handler_t *handler);
-void parse_packet(network_client_t *client, char const *packet, size_t len);
-void zappy_sync_push(session_t client_id, handler_t handler, void *packet);
+	void configure_client_handler(client_handler_t *handler);
+	void parse_packet(network_client_t *client, char const *packet, size_t len);
+	void zappy_sync_push(session_t client_id, handler_t handler, void *packet);
 
-#endif //ZAPPY_LIB_H
+#endif
--- shared/include/util.h
+++ shared/include/util.h
@@ -1,29 +1,29 @@
-//
-// Created by romain on 26/05/18.
-//
+/*
+** EPITECH PROJECT, 2018
+** Zappy
+** File description:
+** Util Header
+*/
 
 #ifndef MYIRC_UTIL_H
-#define MYIRC_UTIL_H
+	#define MYIRC_UTIL_H
 
-#include <stdbool.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <stdint.h>
+	#include <stdbool.h>
+	#include <stdlib.h>
+	#include <string.h>
+	#include <stdio.h>
+	#include <sys/types.h>
+	#include "network_epoll.h"
 
-#define not_split(x)    (!(x) || !(*(x)) || !(x)[1])
+	#define not_split(x) (!(x) || !(*(x)) || !(x)[1])
 
-char **charset_split(char const *str, size_t len, char const *delimiter);
+	char **charset_split(char const *, size_t, char const *);
+	size_t str_array_length(char **);
+	bool charset_ends_with(char const *, size_t, char const *);
+	void str_free_array(char **);
+	bool parse_int(char const *, int64_t *);
+	char *to_string(int);
+	int read_content(int, epoll_t, epoll_config_t, void *);
+	bool parse_uint(char const *, uint32_t *);
 
-size_t str_array_length(char **array);
-
-bool charset_ends_with(char const *str, size_t len, char const *needle);
-
-void str_free_array(char **array);
-
-bool parse_int(char const *str, int64_t *ptr);
-
-bool parse_uint(char const *str, uint32_t *ptr);
-
-char *to_string(int i);
-
-#endif //MYIRC_UTIL_H
+#endif
